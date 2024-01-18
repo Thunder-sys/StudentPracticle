1d52011848bb2129d2394633cf96f73fa198b6d3
admin <kunalwarrior25@gmail.com>
2024-01-18 16:56:10 +0530

    my new activity_kunal.xml

---
+++ app/src/main/java/com/example/studentpracticle/kunal/kunal.kt
@@ -0,0 +1,12 @@
+package com.example.studentpracticle.kunal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.studentpracticle.R
+
+class kunal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kunal)
+    }
+}
