f9a1ba1c1d0e8d1ab926b4364fc8d1b3bc6930af
lappy <shakshik732@gmail.com>
2024-01-18 16:57:07 +0530

    mene ek activity add ki

---
+++ app/src/main/java/com/example/studentpracticle/shakshi/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.studentpracticle.shakshi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.studentpracticle.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
