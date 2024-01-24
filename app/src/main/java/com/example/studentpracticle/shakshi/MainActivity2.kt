7a359e39f5b8cfe25aa6dc0193d486776460fa42
lappy <shakshik732@gmail.com>
2024-01-24 17:09:35 +0530

    i add a new activity_main2.xml

--- app/src/main/java/com/example/studentpracticle/shakshi/MainActivity2.kt
+++ app/src/main/java/com/example/studentpracticle/shakshi/MainActivity2.kt
@@ -2,11 +2,22 @@ package com.example.studentpracticle.shakshi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.example.studentpracticle.R
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        var list=findViewById<ListView>(R.id.mylist)
+        var lists= arrayOf("Agra","Mathura", "Delhi","Ayodhya","Mumbai","Varanasi")
+        val arrayAdapter: ArrayAdapter<*>
+        arrayAdapter= ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            lists
+        )
+        list.adapter=arrayAdapter
     }
 }
