13fbda3c0f953a73e99890f1acd63a4f0bf02b2b
admin <kunalwarrior25@gmail.com>
2024-01-25 16:36:17 +0530

    kunal add button in this Kunal.kt

--- app/src/main/java/com/example/studentpracticle/kunal/Kunal.kt
+++ app/src/main/java/com/example/studentpracticle/kunal/Kunal.kt
@@ -3,27 +3,41 @@ package com.example.studentpracticle.kunal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
 import com.example.studentpracticle.R
 
 class Kunal : AppCompatActivity() {
-
+     private lateinit var listview:ListView
+     private lateinit var top3:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kunal)
 
+        top3 = findViewById(R.id.top3)
 
-        var listview:ListView = findViewById(R.id.kunal_listview)
-        var mycitylist= arrayOf("Mon","Tue","Wed","Thur","Fri","Sat","Sun");
-        var arrayAdapter:ArrayAdapter<*>
+
+        listview = findViewById(R.id.kunal_listview)
+
+
+        val array= arrayOf("Mon","Tue","Wed","Thur","Fri","Sat","Sun")
+        val arrayAdapter:ArrayAdapter<*>
         arrayAdapter= ArrayAdapter(
             this,
             android.R.layout.simple_list_item_1,
-            mycitylist
+            array
         )
         listview.adapter=arrayAdapter
-
-
+        top3.setOnClickListener(){
+            val array3 = arrayOf("Mon","Tue","Wed")
+            val arrayAdapter3:ArrayAdapter<*>
+            arrayAdapter3= ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                array3
+            )
+            listview.adapter=arrayAdapter3
+        }
     }
 }
