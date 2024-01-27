1e03a8b98f3e6bfff6bc410dc88794d1913eb042
kr115 <kr1158721@gmail.com>
2024-01-27 16:42:14 +0530

    i add a list activity i add the background colour i change the button name now i change the name again i add text view i add a beautiful picture of a lady

---
+++ app/src/main/java/com/example/studentpracticle/khushi/khushi/kadvance.kt
@@ -0,0 +1,12 @@
+package com.example.studentpracticle.khushi.khushi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.studentpracticle.R
+
+class kadvance : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kadvance)
+    }
+}
