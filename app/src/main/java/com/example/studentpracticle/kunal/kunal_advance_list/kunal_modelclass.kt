ab55f64b2d737b8657f307718ecf92a3a7f94e37
admin <kunalwarrior25@gmail.com>
2024-01-27 17:02:46 +0530

    kunal create model

---
+++ app/src/main/java/com/example/studentpracticle/kunal/kunal_advance_list/kunal_modelclass.kt
@@ -0,0 +1,13 @@
+package com.example.studentpracticle.kunal.kunal_advance_list
+
+class kunal_modelclass {
+    var kunalActressname:String=""
+    var kunalActressmovie:String=""
+    var kunalimage:Int=0
+
+    constructor(kunalActressname: String, kunalActressmovie: String, kunalimage: Int) {
+        this.kunalActressname = kunalActressname
+        this.kunalActressmovie = kunalActressmovie
+        this.kunalimage = kunalimage
+    }
+}
