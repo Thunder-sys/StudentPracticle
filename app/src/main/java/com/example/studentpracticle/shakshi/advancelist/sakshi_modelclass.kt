d7983edb9fb7f9cab52d9de388a6dfeff806544a
lappy <shakshik732@gmail.com>
2024-01-27 17:02:18 +0530

    i add new activity

---
+++ app/src/main/java/com/example/studentpracticle/shakshi/advancelist/sakshi_modelclass.kt
@@ -0,0 +1,13 @@
+package com.example.studentpracticle.shakshi.advancelist
+
+class sakshi_modelclass {
+    var s_actorname:String=""
+    var s_actormovie:String=""
+    var s_actor:Int=0
+
+    constructor(s_actorname: String, s_actormovie: String, s_actor: Int) {
+        this.s_actorname = s_actorname
+        this.s_actormovie = s_actormovie
+        this.s_actor = s_actor
+    }
+}
