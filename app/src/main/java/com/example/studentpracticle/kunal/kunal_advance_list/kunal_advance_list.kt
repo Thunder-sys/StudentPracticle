60d8f875d2e3eff5ac4372896b8c74dff1581829
admin <kunalwarrior25@gmail.com>
2024-01-27 16:41:51 +0530

    kunal create new activity_kunal_advance_list.xml

---
+++ app/src/main/java/com/example/studentpracticle/kunal/kunal_advance_list/kunal_advance_list.kt
@@ -0,0 +1,12 @@
+package com.example.studentpracticle.kunal.kunal_advance_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.studentpracticle.R
+
+class kunal_advance_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kunal_advance_list)
+    }
+}
