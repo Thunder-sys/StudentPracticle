943e9c30b4162693809e4036874c869d7c5fed52
kr115 <kr1158721@gmail.com>
2024-02-08 15:23:13 +0530

    i update my advance list I add a list

--- app/src/main/java/com/example/studentpracticle/khushi/khushi/khushi/id/id/adpter_idclass.kt
+++ app/src/main/java/com/example/studentpracticle/khushi/khushi/khushi/id/id/adpter_idclass.kt
@@ -1,10 +1,15 @@
 package com.example.studentpracticle.khushi.khushi.khushi.id.id
 
 import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.studentpracticle.R
 
 class adpter_idclass (var context: Activity, var myitem:ArrayList<id_modelclass> ):BaseAdapter(){
     override fun getCount(): Int {
@@ -20,9 +25,33 @@ class adpter_idclass (var context: Activity, var myitem:ArrayList<id_modelclass>
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-       var view:View?
-       var viewholder:ViewHolder?
+        var view: View?
+        var viewholder: ViewHolder?
 
-    if }
+        if (convertView == null) {
+            val myinflator =
+                context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            view = myinflator.inflate(R.layout.activity_khushi_id, null)
+            viewholder = ViewHolder(view)
+            view?.tag = viewholder
+        } else {
+            view = convertView
+            viewholder = view.tag as ViewHolder
+        }
+        var student_id = myitem[position]
+        viewholder.Actorname.text = student_id.Actorname
+        viewholder.Actorfathername.text = student_id.Actorfathername
+        viewholder.Actorsection.text = student_id.Actorsection
+        viewholder.Actorollnumber.text = student_id.Actorrollnumber
+        viewholder.Actorimage.setImageResource(student_id.Actorimage)
+        return view as View
+    }
+    private class ViewHolder(row:View){
+        var Actorname=row.findViewById<TextView>(R.id.text1)
+        var Actorfathername=row.findViewById<TextView>(R.id.text2)
+        var Actorollnumber=row.findViewById<TextView>(R.id.text3)
+        var Actorsection=row.findViewById<TextView>(R.id.text1)
+        var Actorimage=row.findViewById<ImageView>(R.id.image1)
+    }
 
 }
