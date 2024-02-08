5c65ac64a3e3bd5a83f3216344bf9e8fa3c1292e
admin <kunalwarrior25@gmail.com>
2024-02-08 16:23:06 +0530

    kunal correct some activity

--- app/src/main/java/com/example/studentpracticle/khushi/khushi/khushi/id/id/adpter_idclass.kt
+++ app/src/main/java/com/example/studentpracticle/khushi/khushi/khushi/id/id/adpter_idclass.kt
@@ -1,10 +1,16 @@
 package com.example.studentpracticle.khushi.khushi.khushi.id.id
 
 import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.getSystemService
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.studentpracticle.R
 
 class adpter_idclass (var context: Activity, var myitem:ArrayList<id_modelclass> ):BaseAdapter(){
     override fun getCount(): Int {
@@ -21,8 +27,31 @@ class adpter_idclass (var context: Activity, var myitem:ArrayList<id_modelclass>
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
        var view:View?
-       var viewholder:ViewHolder?
+       var viewHolder:ViewHolder?
 
-    if }
+        if (convertView==null){
+            var student= context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            view=student.inflate(R.layout.id_viewlist,null)
+            viewHolder = ViewHolder(view)
+            view?.tag = viewHolder
+        }else{
+            view = convertView
+            viewHolder= view.tag as ViewHolder
+        }
+        var student_id = myitem[position]
+        viewHolder.student_name?.text = student_id.Actorname
+        viewHolder.student_father_name?.text = student_id.Actorfathername
+        viewHolder.student_Roll_Number?.text = student_id.Actorrollnumber
+        viewHolder.student_section?.text = student_id.Actorsection
+        viewHolder.student_image?.setImageResource(student_id.Actorimage)
+        return view as View
+    }
+    private class ViewHolder(row:View){
+        var student_name = row.findViewById<TextView>(R.id.text1)
+        var student_father_name = row.findViewById<TextView>(R.id.text2)
+        var student_Roll_Number = row.findViewById<TextView>(R.id.text3)
+        var student_section = row.findViewById<TextView>(R.id.text4)
+        var student_image = row.findViewById<ImageView>(R.id.image1)
+    }
 
 }
--- app/src/main/java/com/example/studentpracticle/shakshi/s_idcardlist/s_id_modelclass.kt
+++ app/src/main/java/com/example/studentpracticle/shakshi/s_idcardlist/s_id_modelclass.kt
@@ -10,7 +10,7 @@ class s_id_modelclass {
 
     constructor(id_actname:String,id_actfanamme:String,id_sec:String,id_actimag:Int,id_actroll:String){
         this.id_actname = id_actname
-        this.id_actfanamme = id_actfaname
+        this.id_actfanamme = id_actfanamme
         this.id_sec = id_sec
         this.id_actimag = id_actimag
         this.id_actroll = id_actroll
