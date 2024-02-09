382363937f53aa22b7fd1b1faa1b7b2f3e40a937
kr115 <kr1158721@gmail.com>
2024-02-09 15:20:18 +0530

    i update my advance list I add a list getdatalist

--- app/src/main/java/com/example/studentpracticle/khushi/khushi/khushi/id/id/khushi_id_activity.kt
+++ app/src/main/java/com/example/studentpracticle/khushi/khushi/khushi/id/id/khushi_id_activity.kt
@@ -11,23 +11,23 @@ class khushi_id_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_khushi_id)
-        var khus = findViewById<ListView>(R.id.raj)
-        var list_data =adpter_idclass(this,getdataList1())
 
+        var list_data =adpter_idclass(this,getdata())
+        var khus = findViewById<ListView>(R.id.raj)
         khus.adapter = list_data
     }
 
-    private fun getdataList1(): ArrayList<id_modelclass> {
+    private fun getdata(): ArrayList<id_modelclass> {
         var student_datalist = ArrayList<id_modelclass>()
 
-        var student1 = id_modelclass("Name : Akshya Kumar", "Father name : Hari Om Bhatia", "Roll no. : 1", "Section : A", R.drawable.akshay)
-        var student2 = id_modelclass("Name : Ajay", "Father name : Veeru Devgan", "Roll no. : 2", "Section : A", R.drawable.ajay)
-        var student3 = id_modelclass("Name : Divya", "Father name : Jitendra Deshmukh", "Roll no. : 3", "Section : B", R.drawable.divya)
-        var student4 = id_modelclass("Name : Haritik", "Father name : Rakesh Roshan", "Roll no. : 4", "Section : A", R.drawable.hritki)
-        var student5 = id_modelclass("Name : Kirati", "Father name : Rahul Sanon", "Roll no. : 5", "Section : B", R.drawable.kriti)
-        var student6 = id_modelclass("Name : Rao", "Father name : Late Shri G.Venkateswara Rao", "Roll no. : 6", "Section : A", R.drawable.rao)
-        var student7 = id_modelclass("Name : Siddharth", "Father name : Sunil Malhotra", "Roll no. : 7", "Section : A", R.drawable.siddharth)
-        var student8 = id_modelclass("Name : Tanvi", "Father name : Joga Sangha", "Roll no. : 8", "Section : B", R.drawable.tanvi)
+        var student1 = id_modelclass("Akshya Kumar", "Hari Om Bhatia", "1", "A", R.drawable.akshay)
+        var student2 = id_modelclass("Ajay", "Veeru Devgan", "2", "A", R.drawable.ajay)
+        var student3 = id_modelclass("Divya", "Jitendra Deshmukh", "3", "B", R.drawable.divya)
+        var student4 = id_modelclass("Haritik", "Rakesh Roshan", "4", "A", R.drawable.hritki)
+        var student5 = id_modelclass("Kirati", "Rahul Sanon", "5", "B", R.drawable.kriti)
+        var student6 = id_modelclass("Rao", " Late Shri G.Venkateswara Rao", "6", "A", R.drawable.rao)
+        var student7 = id_modelclass("Siddharth", "Sunil Malhotra", "7", "A", R.drawable.siddharth)
+        var student8 = id_modelclass("Tanvi", " Joga Sangha", "8", "B", R.drawable.tanvi)
 
         student_datalist.add(student1)
         student_datalist.add(student2)
